ba1907c09da47441ff431c0b7705b156fad3c77e
LukasLundin <lukas.lundin.1993@gmail.com>
2022-01-28 15:11:25 +0100

    Gradual tutorial. New scenes and edit startingroom

---
+++ Assets/Lukas/Friday_28/MeleeAttack_old.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack_old : MonoBehaviour
+{
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 10;
+
+    public LayerMask enemyLayer;
+    private Animator _animator;
+
+    private Transform _attackPoint;  // testing if lukas formulation works first.
+
+    // testing to se if I can find attackpoint.
+    [SerializeField] private Transform _aimTransform;
+
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _aimTransform = transform.Find("Aim");
+        _attackPoint = _aimTransform.Find("AttackPoint");
+    }
+    void Update()
+    {
+        Attack();
+
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    // Play an attack animation
+        //    _animator.SetTrigger("Attack");
+
+        //    // Detect enemies in range of attack
+        //    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, attackRange, enemyLayer);
+
+        //    // Damage all enemies hit
+        //    foreach (Collider2D enemy in hitEnemies)
+        //    {
+        //        print("Hit: " + enemy.name);
+
+        //        enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        //    }
+        //}
+
+
+    }
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Play an attack animation
+            _animator.SetTrigger("Attack");
+
+            // Detect enemies in range of attack
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, attackRange, enemyLayer);
+
+            // Damage all enemies hit
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                print("Hit: " + enemy.name);
+
+                enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+            }
+        }
+    }
+}
