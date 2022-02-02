54f56d7518c34b10c4533abf17e17c84083c773f
TGJMichael <masern90@gmail.com>
2022-02-02 15:39:58 +0100

    continuation of ranged, most of it works, working on stop moving and start moving functions in both BossRangedAttack and SimpleBossController

--- Assets/BossRangedAttack.cs
+++ Assets/BossRangedAttack.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BossRangedAttack : MonoBehaviour
 {
     private Transform _target;
-    public float range;
+    public float range;   // remove range requirement or change so boss reaches whole room(not into bridge).
     public float knockbackPower = 0.2f;
     public float knockbackDuration = 10f;
 
@@ -29,8 +29,11 @@ public class BossRangedAttack : MonoBehaviour
     // bool that changes the ranged attack from normal to root.
     public bool rootEffect = true;   // should make two methods, one than instantiate rooting projectiles and one that instantiate normal projectiles.
 
-    // Start and stop Chase when in range
-    //private EnemyController _enemyController;     // not needed
+
+    // Stop moving when shooting.. mby should move bool logics here? instead of pulling it from "SimpleBossController"   // not needed if shooting is controlled by S.B.Controller.
+    public bool Moving;
+
+    private SimpleBossController _move;
     void Start()
     {
         _animator = GetComponentInChildren<Animator>();
@@ -38,57 +41,74 @@ public class BossRangedAttack : MonoBehaviour
         _target = FindObjectOfType<CharacterController>().transform;
 
         //_enemyController = GetComponent<EnemyController>();   // not needed.
+        Moving = GetComponent<SimpleBossController>().Moving;
+
+        _move = GetComponent<SimpleBossController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Vector3.Distance(transform.position, _target.transform.position) < range)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            //gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;  // not needed i think, change to someway else to stop boss from moving... Does not work anyway.
-
-            EnemyShoot();
-            //_animator.SetBool("Ranged", true);
+            //GetComponent<SimpleBossController>().Move();
+            _move.Move();
         }
+
+        // Normal shooting activation,, now the shooting is done from "SimpleBossController"
+        //if (Vector3.Distance(transform.position, _target.transform.position) < range)
+        //{
+        //    EnemyShoot();
+        //    //_animator.SetBool("Ranged", true);   // dont think "Ranged" stuff is needed..
+        //}
         //else
         //{
-        //    //gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-
-        //    //_animator.SetBool("Ranged", false);
+        //    //_animator.SetBool("Ranged", false);  
         //}
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            EnemyShoot();
+        }
     }
 
-    private void EnemyShoot()
+    public void EnemyShoot()
     {
         if (Time.time >= nextAttackTime)        // change logic to shoot 3 times then w8 for long time. mby i should have a long timer and break up the animation, loop same exakt frames when spitting while shooting intead of one per animation.
         {
+            _move.StopMove();
             _animator.SetTrigger("Spit");
             //print("Shooting towards " + _target.name);
 
+            //GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+            //Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            //rb.AddForce(firePoint.up * projectileSpeed, ForceMode2D.Impulse);
+
+            nextAttackTime = Time.time + 1f / attackRate;
+
+        }
+        else if (Time.time <= nextAttackTime)
+        {
+
+            _move.Move();
+        }
+    }
+    public void RangedAttackEvent(bool AttackHit)
+    {
+        if (AttackHit)
+        {
             GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
             Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
             rb.AddForce(firePoint.up * projectileSpeed, ForceMode2D.Impulse);
 
-            nextAttackTime = Time.time + 1f / attackRate;
+            //SFX
+            AudioClip clip = spitSounds[UnityEngine.Random.Range(0, spitSounds.Length)];
+            GetComponent<AudioSource>().PlayOneShot(clip);          // something is missing for this to work.
+
+        }
+        else
+        {
+            print("did not shoot?");
         }
     }
-    //public void RangedAttackEvent(bool AttackHit)
-    //{
-    //    if (AttackHit)
-    //    {
-    //        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-    //        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-    //        rb.AddForce(firePoint.up * projectileSpeed, ForceMode2D.Impulse);
-
-    //        //SFX
-    //        AudioClip clip = spitSounds[UnityEngine.Random.Range(0, spitSounds.Length)];
-    //        GetComponent<AudioSource>().PlayOneShot(clip);
-
-    //    }
-    //    else
-    //    {
-    //        print("did not shoot?");
-    //    }
-    //}
 }
